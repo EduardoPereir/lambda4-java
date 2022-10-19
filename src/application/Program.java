01aab34f6fb4145830ed4b1681b79779ed999703
Eduardo Pereira Filho <eduardopereirafilho6@gmail.com>
2022-10-19 10:48:05 -0300

    Function implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,27 @@
 package application;
 
+import entities.Product;
+import util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Mouse", 54.90 ));
+        list.add(new Product("Monitor", 234.93 ));
+        list.add(new Product("Xbox", 1190.99 ));
+        list.add(new Product("Keyboard", 90.99 ));
+
+        UpperCaseName upc = new UpperCaseName();
+
+       List<String> names = list.stream().map(upc).toList();
+       names.forEach(System.out::println);
+
+    }
 }
